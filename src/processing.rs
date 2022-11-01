84f2896f8374d10b02821e2134942c96966ff435
PedroHBrasil <PedroHBrasil7@gmail.com>
2022-11-01 17:41:40 -0300

    Pixels is now a 2D array

--- src/processing.rs
+++ src/processing.rs
@@ -46,56 +46,48 @@ fn gen_min_grad_map(img_gs: &ImageBuffer<LumaA<u8>, Vec<u8>>, n_grad_dir: u32) -
         let y_beg = cmp::max(y as i32 - n_pixel_layers as i32, 0) as u32;
         let y_end = cmp::min(y + n_pixel_layers as u32, y_max);
         // Gets the pixel range
-        let mut pixels: Vec<&LumaA<u8>> = Vec::new();
+        let mut pixels: Vec<Vec<&LumaA<u8>>> = Vec::new();
         for x in x_beg..=x_end {
+            pixels.push(Vec::new());
             for y in y_beg..=y_end {
-                pixels.push(img_gs.get_pixel(x, y));
+                pixels.last_mut().unwrap().push(img_gs.get_pixel(x, y));
             }
         }
         // Determines current pixel's index in pixel range vector
-        let pixels_len = pixels.len() as u32;
-        let i_pixel = det_i_pixel(x, y, x_max, y_max, x_beg, x_end, y_beg, y_end, pixels_len);
+        let n_x = x_end - x_beg + 1;
+        let n_y = y_end - y_beg + 1;
+        let ij_pixel = det_ij_pixel(x, y, x_max, y_max, n_x, n_y);
         // Finds direction of minimal shade gradient
-        dirs.push(find_min_grad_dir(pixels, i_pixel));
+        dirs.push(find_min_grad_dir(pixels, ij_pixel));
     }
 
     dirs
 }
 
 /// Determines the index of the reference pixel in a vector of pixels for the gradient computation.
-fn det_i_pixel(
-    x: u32,
-    y: u32,
-    x_max: u32,
-    y_max: u32,
-    x_beg: u32,
-    x_end: u32,
-    y_beg: u32,
-    y_end: u32,
-    pixels_len: u32,
-) -> u32 {
+fn det_ij_pixel(x: u32, y: u32, x_max: u32, y_max: u32, n_x: u32, n_y: u32) -> (u32, u32) {
     if x == 0 && y == 0 {
-        0 // First element
+        (0, 0) // First pixel
     } else if x == x_max && y == 0 {
-        pixels_len - (x_end - x_beg) // First pixel of last row
+        (n_x - 1, 0) // First pixel of last row
     } else if x == 0 && y == y_max {
-        x_end - x_beg // Last pixel of first row
+        (0, n_y - 1) // Last pixel of first row
     } else if x == x_max && y == y_max {
-        pixels_len - 1
+        (n_x - 1, n_y - 1) // Last pixel of last row
     } else if x == 0 {
-        y - y_beg // yth pixel of first row
+        (0, n_y / 2) // Mid pixel of first row
     } else if y == 0 {
-        (1 + x_end - x_beg) * (y_end - y_beg) / 2 // First column of mid row
+        (n_x / 2, 0) // First pixel of mid row
     } else if x == x_max {
-        (pixels_len + x_end - x_beg) / 2 // Last pixel of mid row
+        (n_x / 2, n_y - 1) // Last pixel of mid row
     } else if y == y_max {
-        pixels_len - 1 - (x_end - x_beg) / 2 // Mid column of last row
+        (n_x - 1, n_y / 2) // Mid column of last row
     } else {
-        pixels_len / 2 // Central pixel
+        (n_x / 2, n_y / 2) // Central pixel
     }
 }
 
-fn find_min_grad_dir(pixel_range: Vec<&LumaA<u8>>, i_pixel: u32) -> u16 {
+fn find_min_grad_dir(pixels: Vec<Vec<&LumaA<u8>>>, ij_pixel: (u32, u32)) -> u16 {
     0
 }
 
