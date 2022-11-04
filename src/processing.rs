16f424e7f73b92d0d2aec180c7ba8861e3384b3d
PedroHBrasil <PedroHBrasil7@gmail.com>
2022-11-04 17:39:30 -0300

    refactored, added calc_grad and added tests

--- src/processing.rs
+++ src/processing.rs
@@ -1,5 +1,5 @@
-use image::{DynamicImage, ImageBuffer, LumaA, Pixel};
-use std::{cmp, f32::consts::PI};
+use image::{DynamicImage, GrayAlphaImage, ImageBuffer, LumaA, Pixel};
+use std::{cmp, f64::consts::PI};
 
 /// Processes an image.
 pub fn process_img(
@@ -16,7 +16,7 @@ pub fn process_img(
 
 /// Finds the indices of the regions that each pixel belongs to. Identification is based on the
 /// spectrum of shades determined by the number of colors input.
-fn find_color_regions(n_shades: u8, img_gs: &ImageBuffer<LumaA<u8>, Vec<u8>>) -> Vec<u8> {
+fn find_color_regions(n_shades: u8, img_gs: &GrayAlphaImage) -> Vec<u8> {
     // Gets vector of shades indices for each pixel
     let step = std::u8::MAX / n_shades;
     let mut i_shades: Vec<u8> = Vec::new();
@@ -30,130 +30,220 @@ fn find_color_regions(n_shades: u8, img_gs: &ImageBuffer<LumaA<u8>, Vec<u8>>) ->
 
 /// Generates the minimum shade gradient map for a grayscale image. This map contains the directions
 /// to where the shade changes less for each pixel, relative to the other analyzed directions.
-fn gen_min_grad_map(img_gs: &ImageBuffer<LumaA<u8>, Vec<u8>>, n_grad_dir: u32) -> Vec<f32> {
+fn gen_min_grad_map(img_gs: &GrayAlphaImage, n_grad_dir: u32) -> Vec<u32> {
     // Finds radius of gradient analysis
     let n_pixel_layers = n_grad_dir / 4 + 1;
     // Generates test directions vector
-    let mut test_directs: Vec<f32> = Vec::new();
+    let mut test_directs = Vec::new();
     for i_dir in 0..n_grad_dir {
-        test_directs.push(i_dir as f32 * PI / n_grad_dir as f32);
+        test_directs.push(i_dir as f64 * PI / n_grad_dir as f64);
     }
-
     // Finds the direction of minimum gradient for each pixel
-    let x_max = img_gs.width() - 1;
-    let y_max = img_gs.height() - 1;
-    let mut min_grad_directs_map: Vec<f32> = Vec::new();
+    let mut min_i_grad_directs_map: Vec<u32> = Vec::new();
     for (x, y, _) in img_gs.enumerate_pixels() {
-        // Calculates coordinates of pixel range to use in gradient calculation
-        let x_beg = cmp::max(x as i32 - n_pixel_layers as i32, 0) as u32;
-        let x_end = cmp::min(x + n_pixel_layers as u32, x_max);
-        let y_beg = cmp::max(y as i32 - n_pixel_layers as i32, 0) as u32;
-        let y_end = cmp::min(y + n_pixel_layers as u32, y_max);
-        // Gets the pixel range
-        let mut pixels: Vec<Vec<&LumaA<u8>>> = Vec::new();
-        for x in x_beg..=x_end {
-            pixels.push(Vec::new());
-            for y in y_beg..=y_end {
-                pixels.last_mut().unwrap().push(img_gs.get_pixel(x, y));
-            }
-        }
-        // Determines current pixel's index in pixel range vector
-        let n_x = x_end - x_beg + 1;
-        let n_y = y_end - y_beg + 1;
-        let ij_pixel = det_ij_pixel(x, y, x_max, y_max, n_x, n_y);
+        let subpixels = get_subpixels(img_gs, x, y, n_pixel_layers);
         // Finds direction of minimal shade gradient
-        min_grad_directs_map.push(find_min_grad_dir(
-            pixels,
-            ij_pixel,
-            &test_directs,
-            n_pixel_layers,
-        ));
+        let i_min_grad_dir = find_i_min_grad_dir(subpixels, &test_directs, n_pixel_layers);
+        min_i_grad_directs_map.push(i_min_grad_dir);
     }
 
-    min_grad_directs_map
+    min_i_grad_directs_map
 }
 
-/// Determines the index of the reference pixel in a vector of pixels for the gradient computation.
-fn det_ij_pixel(x: u32, y: u32, x_max: u32, y_max: u32, n_x: u32, n_y: u32) -> (u32, u32) {
-    if x == 0 && y == 0 {
-        (0, 0) // First pixel
-    } else if x == x_max && y == 0 {
-        (n_x - 1, 0) // First pixel of last row
-    } else if x == 0 && y == y_max {
-        (0, n_y - 1) // Last pixel of first row
-    } else if x == x_max && y == y_max {
-        (n_x - 1, n_y - 1) // Last pixel of last row
-    } else if x == 0 {
-        (0, n_y / 2) // Mid pixel of first row
-    } else if y == 0 {
-        (n_x / 2, 0) // First pixel of mid row
-    } else if x == x_max {
-        (n_x / 2, n_y - 1) // Last pixel of mid row
-    } else if y == y_max {
-        (n_x - 1, n_y / 2) // Mid column of last row
-    } else {
-        (n_x / 2, n_y / 2) // Central pixel
-    }
-}
-
-fn find_min_grad_dir(
-    pixels: Vec<Vec<&LumaA<u8>>>,
-    ij_pixel: (u32, u32),
-    test_directs: &Vec<f32>,
+/// Gets the matrix of subpixels based on the coordinates of the central pixel and the number of
+/// layers.
+fn get_subpixels(
+    img_gs: &GrayAlphaImage,
+    x: u32,
+    y: u32,
     n_pixel_layers: u32,
-) -> f32 {
-    // Finds pixels that compose gradient
-    let mut angles: Vec<Vec<Option<f32>>> = Vec::new();
-    for i in 0..pixels.len() {
-        angles.push(Vec::new());
-        for j in 0..pixels[i].len() {
-            // Prevents invalid operations
-            if (i as u32, j as u32) == ij_pixel {
-                angles[i].push(None);
-                continue;
+) -> Vec<Vec<Option<&LumaA<u8>>>> {
+    // Gets valid coordinate limits
+    let x_max = img_gs.width() as i32 - 1;
+    let y_max = img_gs.height() as i32 - 1;
+    // Calculates coordinates of pixel range to use in gradient calculation
+    let x_beg = x as i32 - n_pixel_layers as i32;
+    let x_end = x as i32 + n_pixel_layers as i32;
+    let y_beg = y as i32 - n_pixel_layers as i32;
+    let y_end = y as i32 + n_pixel_layers as i32;
+    // Gets the pixel range
+    let mut subpixels: Vec<Vec<Option<&LumaA<u8>>>> = Vec::new();
+    for x_i in x_beg..=x_end {
+        subpixels.push(Vec::new());
+        for y_i in y_beg..=y_end {
+            if x_i < 0 || x_i > x_max || y_i < 0 || y_i > y_max {
+                subpixels.last_mut().unwrap().push(None);
+            } else {
+                let pixel = img_gs.get_pixel(x_i as u32, y_i as u32);
+                subpixels.last_mut().unwrap().push(Some(pixel));
             }
-            // Finds relative angle for current pixel
-            let i_delta = i as u32 - ij_pixel.0;
-            let j_delta = j as u32 - ij_pixel.1;
-            let angle = (i_delta as f32).atan2(j_delta as f32);
-            angles[i].push(Some(angle));
         }
     }
-    let mut grads: Vec<f32> = Vec::new();
-    // Finds the gradients in every test direction
-    for direct in test_directs {
-        grads.push(calc_grad(&pixels, direct, &n_pixel_layers));
-    }
 
-    grads[0]
+    subpixels
 }
 
-fn calc_grad(pixels: &Vec<Vec<&LumaA<u8>>>, direct: &f32, n_pixel_layers: &u32) -> f32 {
-    // Selects pixels that compose gradient of the given direction
-    let ij_pixels = get_pixels_in_line(direct, n_pixel_layers);
-    // Computes
-    0.0
+/// Finds the index of the direction with the lowest color gradient value for a given pixel set.
+fn find_i_min_grad_dir(
+    subpixels: Vec<Vec<Option<&LumaA<u8>>>>,
+    test_directs: &Vec<f64>,
+    n_pixel_layers: u32,
+) -> u32 {
+    // Finds the gradients in every test direction
+    let mut grads = Vec::new();
+    for direct in test_directs {
+        grads.push(calc_grad(&subpixels, *direct, n_pixel_layers));
+    }
+    // Gets index of direction of minimal gradient
+
+    0
+}
+
+/// Computes the color gradient for a pixel set in a given direction.
+fn calc_grad(subpixels: &Vec<Vec<Option<&LumaA<u8>>>>, direct: f64, n_pixel_layers: u32) -> f64 {
+    // Selects pixels that compose the gradient of the given direction
+    let ij_pixels = get_pixels_in_line(direct, n_pixel_layers as i32);
+    // Computes the gradient value
+    let n_subpixels = n_pixel_layers * 2 + 1;
+    let mut shade_sum = 0.0;
+    let mut n_shades = 0.0;
+    for i_subpixel in 0..n_subpixels {
+        let (i, j) = ij_pixels[i_subpixel as usize];
+        let subpixel_opt = subpixels[i as usize][j as usize];
+        if let Some(subpixel) = subpixel_opt {
+            let shade = subpixel.0[0];
+            shade_sum += shade as f64;
+            n_shades += 1.0;
+        }
+    }
+    let grad = shade_sum / n_shades;
+
+    grad
 }
 
 /// Gets a vector of pixel coordinates that are in the directions specified. Reference is a central
 /// pixel of the pixel matrix of n_pixel_layers surrounding it.
-fn get_pixels_in_line(direct: &f32, n_pixel_layers: &u32) -> Vec<(u32, u32)> {
+fn get_pixels_in_line(direct: f64, n_pixel_layers: i32) -> Vec<(u32, u32)> {
     let mut ij_pixels: Vec<(u32, u32)> = Vec::new();
-    for i_layer in 1..*n_pixel_layers {
+    // Coordinates of central (reference) subpixel
+    let ij_center = n_pixel_layers;
+    // Going from -n_layers to +n_layers inclusive allows for sorted insertion in vec
+    for i_layer in -n_pixel_layers..=n_pixel_layers {
         // Avoids division by 0 (and small numbers)
-        let ij = if direct < &(PI / 4.0) || direct > &(PI * 3.0 / 4.0) {
+        if direct <= (PI / 4.0) || direct > (PI * 3.0 / 4.0) {
             // Multiplies using the tangent value directly (between -1 and 1)
-            let i = i_layer;
-            let j = (i_layer as f32 * direct.tan()).round() as u32;
-            (i, j)
+            let i = ij_center - (i_layer as f64 * direct.tan()).round() as i32;
+            let j = ij_center + i_layer;
+            ij_pixels.push((i as u32, j as u32));
         } else {
             // Multiplies using the cotangent value (cos/sin) (between -1 and 1)
-            let i = (i_layer as f32 * direct.cos() / direct.sin()).round() as u32;
-            let j = i_layer;
-            (i, j)
-        };
-        ij_pixels.push(ij);
+            let i = ij_center + i_layer;
+            let j = ij_center - (i_layer as f64 * direct.cos() / direct.sin()).round() as i32;
+            ij_pixels.push((i as u32, j as u32));
+        }
     }
 
     ij_pixels
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn img_factory(width: u32, height: u32, grad_dir: f64) -> GrayAlphaImage {
+        // Finds ratios and virtual image sizes
+        let x_ratio = grad_dir.cos() / (grad_dir.cos() + grad_dir.sin());
+        let y_ratio = grad_dir.sin() / (grad_dir.cos() + grad_dir.sin());
+        let (width_virt, height_virt) = (width as f64 * x_ratio, height as f64 * y_ratio);
+        // Makes image buffer
+        let img = ImageBuffer::from_fn(width, height, |x, y| -> LumaA<u8> {
+            // Makes pixel according to the gradient direction pattern
+            let (x_virt, y_virt) = (x as f64 * x_ratio, y as f64 * y_ratio);
+            let ratio = (x_virt + y_virt) / (width_virt + height_virt);
+            let shade = (std::u8::MAX as f64 * ratio) as u8;
+            LumaA([shade; 2])
+        });
+
+        img
+    }
+
+    #[test]
+    fn get_subpixels_5x5_x0_y0_n1() {
+        let x = 0;
+        let y = 0;
+
+        let img_gs: GrayAlphaImage = img_factory(5, 5, 3.0 * PI / 4.0);
+
+        let expected = vec![
+            vec![None, None, None],
+            vec![
+                None,
+                Some(img_gs.get_pixel(0, 0)),
+                Some(img_gs.get_pixel(0, 1)),
+            ],
+            vec![
+                None,
+                Some(img_gs.get_pixel(1, 0)),
+                Some(img_gs.get_pixel(1, 1)),
+            ],
+        ];
+        let result = get_subpixels(&img_gs, x, y, 1);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn get_pixels_in_line_dir_0() {
+        let direct = 0.0;
+        let n_pixel_layers = 2;
+
+        let expected: Vec<(u32, u32)> = vec![(2, 0), (2, 1), (2, 2), (2, 3), (2, 4)];
+        let result = get_pixels_in_line(direct, n_pixel_layers);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn get_pixels_in_line_dir_pi4() {
+        let direct = PI / 4.0;
+        let n_pixel_layers = 2;
+
+        let expected: Vec<(u32, u32)> = vec![(4, 0), (3, 1), (2, 2), (1, 3), (0, 4)];
+        let result = get_pixels_in_line(direct, n_pixel_layers);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn get_pixels_in_line_dir_pi2() {
+        let direct = PI / 2.0;
+        let n_pixel_layers = 2;
+
+        let expected: Vec<(u32, u32)> = vec![(0, 2), (1, 2), (2, 2), (3, 2), (4, 2)];
+        let result = get_pixels_in_line(direct, n_pixel_layers);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn get_pixels_in_line_dir_pi34() {
+        let direct = 3.0 * PI / 4.0;
+        let n_pixel_layers = 2;
+
+        let expected: Vec<(u32, u32)> = vec![(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)];
+        let result = get_pixels_in_line(direct, n_pixel_layers);
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn get_pixels_in_line_dir_pi() {
+        let direct = PI;
+        let n_pixel_layers = 2;
+
+        let expected: Vec<(u32, u32)> = vec![(2, 0), (2, 1), (2, 2), (2, 3), (2, 4)];
+        let result = get_pixels_in_line(direct, n_pixel_layers);
+
+        assert_eq!(expected, result);
+    }
 }
